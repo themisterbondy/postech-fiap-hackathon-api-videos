c3501f050a6b98d369b02aaf2bbc416f1aecfd39
Camila Nachbar <camila.nachbar@gmail.com>
2025-04-11 22:23:55 -0300

    Add contracts

---
+++ src/Postech.Fiap.Hackathon.VideoProcessing.WebApi/Features/Videos/Contracts/DownloadVideoZipRequest.cs
@@ -0,0 +1,7 @@
+namespace Postech.Fiap.Hackathon.VideoProcessing.WebApi.Features.Videos.Contracts;
+
+public class DownloadVideoZipRequest
+{
+    public string? id { get; set; }
+
+}
---
+++ src/Postech.Fiap.Hackathon.VideoProcessing.WebApi/Features/Videos/Contracts/DownloadVideoZipResponse.cs
@@ -0,0 +1,7 @@
+namespace Postech.Fiap.Hackathon.VideoProcessing.WebApi.Features.Videos.Contracts;
+
+public class DownloadVideoZipResponse
+{
+    public IFormFile? File { get; set; }
+
+}
